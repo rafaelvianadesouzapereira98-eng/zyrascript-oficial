bd3791c3177fde725b5dec181f50a245c1da9ba8
rafaelvianadesouzapereira98-eng <rafaelvianadesouzapereira98@gmail.com>
2025-11-29 11:18:12 -0300

    Update version constant and welcome message

--- zyra.js
+++ zyra.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-const VERSAO_ZYRA = "4.0.0"; 
+const VERSAO_HYGEA = "4.0.0"; 
 
 function executarNoTerminal() {
     const args = process.argv.slice(2);
     if (args.includes('--versao') || args.includes('-v')) {
-        console.log(`ZyraScript Versão ${VERSAO_ZYRA}`);
+        console.log(`ZyraScript Versão ${VERSAO_HYGEA}`);
         return;
     }
     console.log("Bem-vindo ao Zyra CLI. Use '--versao' ou '-v' para ver a versão.");
 }
+
 
 executarNoTerminal();
